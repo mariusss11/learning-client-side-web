103f23aba3ced4013d6fb9d1a8333473a00e3bbc
mariusss11 <mariuscarchilan07@gmail.com>
2026-04-26 15:29:39 +0300

    Add the responsive version of the home page

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+
+console.log("100 - \"abc\"  = ", 100 - "abc")
+console.log("100 + \"abc\"  = ", 100 + "abc")
+
+let name;
+console.log(name)
+
+let nullValue = null;
+console.log(nullValue)
+
+let a = "CEITI este un colegiu";
+
+console.log(a.substring(0, 5))
+
+// this replace just the first occurence
+console.log(a.replace("CEITI", "CUTM"))
+// use replaceAll to replace all
+
+console.log(NaN === NaN)
+
+
+
